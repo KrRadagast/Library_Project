27640d027bd9a287bde2b8ad6b3b639a749bc0a3
Nicholas Poehlman <nicholas.poehlman343@gmail.com>
2023-04-21 11:45:01 -0700

    done

--- app.js
+++ app.js
@@ -1,4 +1,78 @@
-console.log("Hello World!\n==========\n");
+
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+const books = (
+    {
+        id:1,
+        title:"name of the wind",
+        author:"Patrick Rothfuss",
+        read:true,
+    }
+)
+    
+
+class Book{
+    constructor(id,title,author,read){
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.read=read;
+    }
+}
+class Library{
+    books=[];
+    addBook(){
+        const title=document.getElementById("title");
+        const author=document.getElementById("author");
+        const read=document.getElementById("read");
+        this.nextId++;
+        this.bookCount++;
+        console.log("Title" + title.value +author.value +read.value)
+        const newBook=new Book(this.nextId, title.value, author.value, read.value);
+        
+        this.books.push(newBook);
+
+        
+        console.log(this.books,this.bookCount);
+
+        const tbody=document.getElementById("tableBody");
+        
+        const newTr=document.createElement("tr");
+        
+        const newTitle=document.createElement("td");
+        const newAuthor=document.createElement("td");
+        const newRead=document.createElement("td");
+        
+        newTitle.textContent=title.value;
+        newAuthor.textContent=author.value;
+        const newCheckbox=document.createElement("input");
+        newCheckbox.type="checkbox";
+        newCheckbox.checked=read.checked;
+        newCheckbox.disabled=read.checked;
+        newCheckbox.addEventListener("click",(event)=>{
+            this.markRead(event.target,parseInt(event.target.id))
+        })
+        newRead.appendChild(newCheckbox);
+        newTr.appendChild(newTitle);
+        newTr.appendChild(newAuthor);
+        newTr.appendChild(newRead);
+        tbody.appendChild(newTr);
+    }
+    markRead(checkbox,id){
+        this.books.forEach((book)=>{
+            if(id===book.id){
+                book.read=true;
+                checkbox.checked=true;
+                checkbox.disabled=true;
+            }
+        });
+    }
+}
+const library=new Library(books);
+const form=document.getElementById("form");
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    library.addBook();
+})
